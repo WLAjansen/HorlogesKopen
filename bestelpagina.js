52cbc1b337afe4d8aaec607ab824b5a48a1acec1
WLAjansen <40787982+WLAjansen@users.noreply.github.com>
2020-02-01 17:31:22 +0100

    Update 7
    Bestelpagina aangepast

--- bestelpagina.js
+++ bestelpagina.js
@@ -38,15 +38,13 @@ let winkelwagen = {
       bestelling = JSON.parse(localStorage.getItem('BesteldeHorloges'));
       document.querySelector('.winkelwagen__aantal').innerHTML = bestelling.length;
     }
+    bestelling.forEach( item => {
+      this.items.push(item);
+    })
     return bestelling;
   },
 
-  toevoegen: function(el) {
-    this.items = this.haalItemsOp()
-    this.items.push(el);
-    localStorage.
-    document.querySelector('.winkelwagen__aantal').innerHTML = this.items.length;
-  },
+
 
   // de data in een tabel uitvoeren
   uitvoeren: function() {
@@ -54,53 +52,34 @@ let winkelwagen = {
     document.getElementById('uitvoer').innerHTML = "";
     this.items.forEach( horloge => {
       let sectie = document.createElement('section');
-      sectie.className = 'horlogeSelectie';
+      sectie.className = 'besteldHorloge';
       // main element met alle info behalve prijs en afbeelding
       let main = document.createElement('main');
-      main.className = 'horlogeSelectie__main';
+      main.className = 'besteldHorloge__main';
 
       // cover maken
       let afbeelding = document.createElement('img');
-      afbeelding.className = 'horlogeSelectie__cover';
+      afbeelding.className = 'besteldHorloge__cover';
       afbeelding.setAttribute('src', horloge.cover);
       afbeelding.setAttribute('alt', horloge.model);
 
       // model naam maken voor het horloge
       let model = document.createElement('h3');
-      model.className = 'horlogeSelectie__model';
+      model.className = 'besteldHorloge__model';
       model.textContent = horloge.model;
-
-      // merk toevoegen
-      let merk = document.createElement('p');
-      merk.className = 'horlogeSelectie__merken';
-      merk.textContent = horloge.merk;
-
-      // overige info toevoegen
-      let overig = document.createElement('p');
-      overig.className = 'horlogeSelectie__overig';
-      overig.textContent = 'datum: '+horloge.uitgave+' | maat: '+horloge.maat+' | kleur: '+horloge.kleur+' | serienummer: '+horloge.ean;
 
       // prijs toevoegen
       let prijs = document.createElement('div');
-      prijs.className = 'horlogeSelectie__prijs';
+      prijs.className = 'besteldHorloge__prijs';
       prijs.textContent = horloge.prijs.toLocaleString('nl-NL', {currency: 'EUR', style: 'currency'});
 
-      // knop toevoegen bij de Prijs
-      let knop = document.createElement('button');
-      knop.className = 'horlogeSelectie__knop';
-      knop.innerHTML = 'Koop';
-      knop.addEventListener('click', () => {
-        winkelwagen.toevoegen(horloge);
-      })
+      // verwijderknop toevoegen
 
       // de element toevoegen
       sectie.appendChild(afbeelding);
       main.appendChild(model);
-      main.appendChild(merk);
-      main.appendChild(overig);
       sectie.appendChild(main);
       sectie.appendChild(prijs);
-      prijs.appendChild(knop);
       document.getElementById('uitvoer').appendChild(sectie);
     });
   }
